6c103957584d8bfa8c4dfd5cf66af61f13283e18
Oreda-Z <shtucer31@gmail.com>
2022-07-12 10:59:24 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, {Request, Response} from 'express'
+import cors from 'cors'
+import bodyParser from "body-parser";
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(bodyParser.json())
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`server run on port ${port}`)
+})
